fd8a9fba19c1eea4ee8683fff781dc90e726e18e
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-05-18 16:51:08 +0100

    fix: {} errors

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,9 +76,9 @@ function checkWinner() {
     for (let c = 0; c < columns - 3; c++) {
       if (board[r][c] != " ") {
         if (
-          board[r][c] == board[r][c + 1] &&
-          board[r][c + 1] == board[r][c + 2] &&
-          board[r][c + 2] == board[r][c + 3]
+          board[r][c] == board[r + 1][c] &&
+          board[r + 1][c] == board[r + 2][c] &&
+          board[r + 2][c] == board[r + 3][c]
         ) {
           setwinner(r, c);
           return;
@@ -98,8 +98,9 @@ function checkWinner() {
           setwinner(r, c);
           return;
         }
+      }
     }
-}
+  }
 }
 
 function setwinner(r, c) {
