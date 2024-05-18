f91687c58946be9f1d5666ab5d6ac3c1358de834
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-05-18 17:00:19 +0100

    feat: Anti diagonally added, C&P from horizotnal.

--- assets/js/game.js
+++ assets/js/game.js
@@ -101,6 +101,21 @@ function checkWinner() {
       }
     }
   }
+  //Anti diagonally
+  for (let r = 0; r < rows - 3; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r + 1][c + 1] &&
+          board[r + 1][c + 1] == board[r + 2][c + 2] &&
+          board[r + 2][c + 2] == board[r + 3][c + 3]
+        ) {
+          setwinner(r, c);
+          return;
+        }
+      }
+    }
+  }
 }
 
 function setwinner(r, c) {
