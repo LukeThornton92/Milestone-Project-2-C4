436dffead8aca934dd12066db3eb943a9ed561d6
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-05-18 14:34:14 +0100

    chore: setGame function creation

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,6 +8,21 @@ let board;
 const rows = 6;
 const columns = 7;
 
+//sets the game on the window loading.
 window.onload = function () {
   setGame();
 };
+
+function setGame() {
+  board = [];
+
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+      //JS, pushes empty string.
+      row.push(" ");
+      //HTML, creates new div element.
+      let tile = document.createElement("div");
+    }
+  }
+}
