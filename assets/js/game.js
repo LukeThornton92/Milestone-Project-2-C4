f73dd7c5ec0fc30d57bf7f7105c8528ec82cdc8c
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-05-18 17:02:15 +0100

    feat: diagonal check added

--- assets/js/game.js
+++ assets/js/game.js
@@ -116,6 +116,21 @@ function checkWinner() {
       }
     }
   }
+  //Diagonally
+  for (let r = 3; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r - 1][c + 1] &&
+          board[r - 1][c + 1] == board[r - 2][c + 2] &&
+          board[r - 2][c + 2] == board[r - 3][c + 3]
+        ) {
+          setwinner(r, c);
+          return;
+        }
+      }
+    }
+  }
 }
 
 function setwinner(r, c) {
